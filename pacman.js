23ff328f75fdb8859a7f55b62d0ace0b94ddc140
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-08-26 13:46:13 -0400

    Created final assignment

--- pacman.js
+++ pacman.js
@@ -1,41 +1,41 @@
 // Setup initial game stats
 let score = 0;
 let lives = 2;
-
+let powerPellets = 4;
 
 // Define your ghosts here
 const inky = {
-  menu_option: '1',
-  name: 'Inky',
-  colour: 'Red',
-  character: 'Shadow',
-  edible: false
+  menu_option: "1",
+  name: "Inky",
+  colour: "Red",
+  character: "Shadow",
+  edible: false]
+  
 };
 const blinky = {
-  menu_option: '2',
-  name: 'Blinky',
-  colour: 'Cyan',
-  character: 'Speedy',
+  menu_option: "2",
+  name: "Blinky",
+  colour: "Cyan",
+  character: "Speedy",
   edible: false
 };
 const pinky = {
-  menu_option: '3',
-  name: 'Pinky',
-  colour: 'Pink',
-  character: 'Bashful',
+  menu_option: "3",
+  name: "Pinky",
+  colour: "Pink",
+  character: "Bashful",
   edible: false
 };
-const Clyde = {
-  menu_option: '4',
-  name: 'Clyde',
-  colour: 'Orange',
-  character: 'Pokey',
+const clyde = {
+  menu_option: "4",
+  name: "Clyde",
+  colour: "Orange",
+  character: "Pokey",
   edible: false
 };
-
 
 // replace this comment with your four ghosts setup as objects
-
+const ghosts = [inky, blinky, pinky, clyde];
 
 // Draw the screen functionality
 function drawScreen() {
@@ -48,47 +48,126 @@ function drawScreen() {
 }
 
 function clearScreen() {
-  console.log('\x1Bc');
+  console.log("\x1Bc");
 }
 
 function displayStats() {
-  console.log(`Score: ${score}     Lives: ${lives}`);
+  console.log(
+    `Score: ${score}     Lives: ${lives}\n\n\nPower-Pellets: ${powerPellets}`
+  );
 }
 
 function displayMenu() {
-  console.log('\n\nSelect Option:\n');  // each \n creates a new line
-  console.log('(d) Eat Dot');
-  console.log('(q) Quit');
+  console.log("\n\nSelect Option:\n"); // each \n creates a new line
+  console.log("(d) Eat Dot");
+  if (powerPellets > 0) {
+    console.log("(p) Eat Power-Pellet");
+  }
+  if (inky["edible"]) {
+    console.log("(1) Eat Inky (edible)");
+  } else if (inky["edible"] === false) {
+    console.log("(1) Eat Inky (inedible)");
+  }
+  if (blinky["edible"]) {
+    console.log("(2) Eat Blinky (edible)");
+  } else if (blinky["edible"] === false) {
+    console.log("(2) Eat Blinky (inedible)");
+  }
+  if (pinky["edible"]) {
+    console.log("(3) Eat Pinky (edible)");
+  } else if (pinky["edible"] === false) {
+    console.log("(3) Eat Pinky (inedible)");
+  }
+  if (clyde["edible"]) {
+    console.log("(4) Eat Clyde (edible)");
+  } else if (clyde["edible"] === false) {
+    console.log("(4) Eat Clyde (inedible)");
+  }
+  console.log("(q) Quit");
 }
 
 function displayPrompt() {
   // process.stdout.write is similar to console.log except it doesn't add a new line after the text
-  process.stdout.write('\nWaka Waka :v '); // :v is the Pac-Man emoji.
+  process.stdout.write("\nWaka Waka :v "); // :v is the Pac-Man emoji.
 }
-
 
 // Menu Options
 function eatDot() {
-  console.log('\nChomp!');
+  console.log("\nChomp!");
   score += 10;
 }
 
-
 // Process Player's Input
 function processInput(key) {
-  switch(key) {
-    case '\u0003': // This makes it so CTRL-C will quit the program
-    case 'q':
+  switch (key) {
+    case "\u0003": // This makes it so CTRL-C will quit the program
+    case "q":
       process.exit();
       break;
-    case 'd':
+    case "d":
       eatDot();
       break;
+    case "p":
+      if (powerPellets > 0) {
+        eatPowerPellet();
+      } else {
+        console.log("\nNo Power-Pellets left!");
+      }
+      break;
+    case "1":
+      eatGhost(inky);
+      break;
+    case "2":
+      eatGhost(blinky);
+      break;
+    case "3":
+      eatGhost(pinky);
+      break;
+    case "4":
+      eatGhost(clyde);
+      break;
     default:
-      console.log('\nInvalid Command!');
+      console.log("\nInvalid Command!");
   }
 }
 
+const eatGhost = ghost => {
+  if (ghost["edible"] === false) {
+    lives -= 1;
+    console.log(
+      "\n" +
+        ghost["name"] +
+        " with " +
+        ghost["colour"] +
+        " colour kills PacMan!"
+    );
+  } else {
+    console.log(
+      "\n" +
+        ghost["name"] +
+        " with " +
+        ghost["character"] +
+        " character was eaten!"
+    );
+    score += 200;
+    ghost["edible"] = false;
+  }
+  endGame();
+};
+
+const endGame = () => {
+  if (lives < 0) {
+    process.exit();
+  }
+};
+
+const eatPowerPellet = () => {
+  score += 50;
+  for (let ghost of ghosts) {
+    ghost["edible"] = true;
+  }
+  powerPellets -= 1;
+};
 
 //
 // YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
@@ -98,19 +177,19 @@ function processInput(key) {
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
-stdin.setEncoding('utf8');
+stdin.setEncoding("utf8");
 
 // Draw screen when game first starts
 drawScreen();
 
 // Process input and draw screen each time player enters a key
-stdin.on('data', (key) => {
+stdin.on("data", key => {
   process.stdout.write(key);
   processInput(key);
   setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
-process.on('exit', () => {
-  console.log('\n\nGame Over!\n');
+process.on("exit", () => {
+  console.log("\n\nGame Over!\n");
 });
